c968a1dc23cf75353b520592cad94558bfb8ffa0
Maaz Shaikh <mohdmaaz.612054.it@mhssce.ac.in>
2023-06-22 02:31:57 +0530

    First Repo

---
+++ dowhileloop.c
@@ -0,0 +1,17 @@
+//Printing numbers in a sequence.
+#include <stdio.h>
+
+int main()
+{
+    int num, index = 0;
+    printf("Enter a number\n");
+    scanf("%d", &num);
+
+    do
+    {
+        printf("%d\n", index);
+        index = index + 1;
+    } while (index < num);
+
+    return 0;
+}
